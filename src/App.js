2473554e6dd623debd98678fcde28485eded3c78
Nattakorn Sanpabopit <nattakorn.kingman@gmail.com>
2017-08-11 18:09:29 +0700

    adding api key

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import UploadComp from './components/s3UploadComp'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          {/* <h2>Welcome to React</h2> */}
         </div>
-        <p className="App-intro">
+        {/* <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        <UploadComp />
       </div>
     );
   }
--- src/components/s3UploadComp.js
+++ src/components/s3UploadComp.js
@@ -2,15 +2,21 @@ var React = require('react');
 var Dropzone = require('react-dropzone');
 var axios = require('axios');
 //https://cors-anywhere.herokuapp.com/
-const ENDPOINT_TO_GET_SIGNED_URL = 'https://cors-anywhere.herokuapp.com/https://vwl1hxrqje.execute-api.us-east-1.amazonaws.com/dev/rekognition/signUrl';
-
+//GET : https://vwl1hxrqje.execute-api.us-east-1.amazonaws.com/dev/rekognition/signUrl/nattakorn.png/image/png
+const ENDPOINT_TO_GET_SIGNED_URL = 'https://vwl1hxrqje.execute-api.us-east-1.amazonaws.com/dev/rekognition/signUrl';
+const API_KEY = "8eJoc92aMh1Axx3n8x2ya1X3jBgiIQmK7XjiwfLT";
 exports = module.exports = React.createClass({
   _onDrop: function (files) {
     var file = files[0];
-
-    axios.get(ENDPOINT_TO_GET_SIGNED_URL, {
-      filename: file.name,
-      filetype: file.type
+    // let endpointUrl = `${ENDPOINT_TO_GET_SIGNED_URL}/"${file.type}"`
+    axios.get(ENDPOINT_TO_GET_SIGNED_URL,{
+      headers: {
+        "x-api-key":API_KEY
+      },
+      params: {
+        filename: file.name,
+        filetype: file.type
+      }
     })
     .then(function (result) {
       console.log("result : ",result);
@@ -21,10 +27,12 @@ exports = module.exports = React.createClass({
       // console.log("signedUrl :  ",result.data.body.input.signedUrl);
       var options = {
         headers: {
+          'Accept': '*/*',
+          // 'x-amz-acl': 'public-read',
           'Content-Type': file.type
         }
       };
-      console.log("Content-Type : ", file.type);
+      // console.log("Content-Type : ", file.type);
       return axios.put(signedUrl, file, options);
     })
     .then(function (result) {
